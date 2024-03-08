cfff091c57fa79bbea34252f72a0b0f546bf0c6e
oseias-romeiro <oseiasromeirom@gmail.com>
2024-03-08 17:42:41 -0300

    test

--- auth/loaders.py
+++ auth/loaders.py
@@ -4,4 +4,4 @@ from app import login_manager
 @login_manager.user_loader
 def load_user(uid):
     if not uid or uid == "None": return None
-    return User.query.get(int(uid))
+    return User.query.session.get(User, uid)
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,39 +1,73 @@
 import unittest
 from app import app
 
-routes = ['/', '/account/signin', '/account/create']
-
 class TestApp(unittest.TestCase):
 
-    def setUp(self) -> None:
-        # set app as flask instance
+    def setUp(self):
         app.testing = True
         self.app = app.test_client()
+        self.app.application.config['WTF_CSRF_ENABLED'] = False
 
-        return super().setUp()
-    
-    def test_codes(self):
-        for route in self.app.application.url_map.iter_rules():
-            
-            # route.methods
-            if not route.rule.startswith('/static') and not route.rule.startswith('/bootstrap'):
-                print(route.rule)
-                response = self.app.get(route.rule)
-                self.assertNotEqual(response._status_code, 404)
+        self.signup('test', 'test@test.com', 'Test#1234')
+        
 
-    def test_content_length(self):
+    def tearDown(self):
+        pass
+
+    def login(self, username, password):
+        return self.app.post('/account/signin', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def signup(self, username, email, password):
+        return self.app.post('/account/create', data=dict(
+            username=username,
+            email=email,
+            password=password,
+            confirm=password
+        ), follow_redirects=True)
+
+    def test_routes(self):
+        print("\n# Testing response codes and content length")
+
+        routes = {'/': False, '/account/signin': False, '/account/create': False, '/account/home': True, '/account/profile': True}
+        static_paths = ['/static/media/flask-icon.png', '/static/media/flask-logo.png']
+
         for route in routes:
-            response = self.app.get(route)
-            self.assertNotEqual(response.calculate_content_length(), 0)
+            with self.subTest(route=route):
+                if routes[route]:
+                    response = self.app.get(route)
+                    self.assertEqual(response.status_code, 401)
+                    response = self.login('test', 'Test#1234')
+                    self.assertEqual(response.status_code, 200)
+                else: 
+                    response = self.app.get(route)
+                    self.assertEqual(response.status_code, 200)
+                    self.assertNotEqual(len(response.data), 0)
+
+        for path in static_paths:
+            with self.subTest(path=path):
+                with self.app.get(path) as response:
+                    self.assertEqual(response.status_code, 200)
+                    self.assertNotEqual(len(response.data), 0)
 
     def test_index(self):
-        response = self.app.get(routes[0])
-        self.assertIn(b'Flask template', response.data)
+        response = self.app.get('/')
+        self.assertIn(b'<title>Flask template</title>', response.data)
 
     def test_signin(self):
-        response = self.app.get(routes[1])
-        self.assertIn(b'Sign in', response.data)
+        response = self.app.get('/account/signin')
+        self.assertIn(b'<title>Sign in</title>', response.data)
 
     def test_create(self):
-        response = self.app.get(routes[2])
-        self.assertIn(b'Sign up', response.data)
+        response = self.app.get('/account/create')
+        self.assertIn(b'<title>Sign up</title>', response.data)
+    
+    def test_home(self):
+        response = self.login('test', 'Test#1234')
+        response = self.app.get('/account/home')
+        self.assertIn(b'<title>Wellcome</title>', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
